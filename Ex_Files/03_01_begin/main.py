a59010ef7483ba1e8c8ac99eb415811c658e9442
nafisahR <122411544+nafisahR@users.noreply.github.com>
2023-02-02 21:05:41 +0000

    completed 0301

--- Ex_Files/03_01_begin/main.py
+++ Ex_Files/03_01_begin/main.py
@@ -3,6 +3,7 @@ from pprint import pprint
 
 EINSTEIN_CSV = 'Albert,Einstein,1879-03-14,1955-04-18,Germany,"for his services to Theoretical Physics, and especially for his discovery of the law of the photoelectric effect",physics,1921'
 
+#convert Einstein_csv to Einstein
 EINSTEIN = {
     "birthplace": "Germany",
     "name": "Albert",
@@ -11,11 +12,12 @@ EINSTEIN = {
     "category": "physics",
     "motivation": "for his services to Theoretical Physics...",
 }
-
+# open the CSV file to read (r) and assigned to variable (f))
 with open("laureates.csv", "r") as f:
     reader = csv.DictReader(f)
     laureates = list(reader)
 
+#for loop variable (laureate) reads through the entire csv file
 for laureate in laureates:
     if laureate["surname"] == "Einstein":
         pprint(laureate)
